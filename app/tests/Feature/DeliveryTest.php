c4833f2c0c9e58e0815f01cbbad6efcda214f9b1
sulikovaa <ahmetovaa6991@gmail.com>
2025-10-08 17:56:50 +0300

    Update DeliveryTest.php

--- app/tests/Feature/DeliveryTest.php
+++ app/tests/Feature/DeliveryTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\Book;
 
-class DeliveryTests extends TestCase
+class DeliveryTest extends TestCase
 {
     public function test_make_order() : void 
     {
@@ -16,7 +16,15 @@ class DeliveryTests extends TestCase
         $responseData = $response->json();
         $newBook = Book::factory()->create();
         $response = $this
-              ->post('api/cart/' . $newBook->id, ['count'=>1]);
-        $this ->assertEquals(true, $responseData['success']);
+            ->withHeader('Authorization', 'Bearer ' . $responseData['token'])
+            ->post('api/cart/' . $newBook->id, ['count'=>1]);
+        $responseDataAddToCart = $response->json();
+        $this->assertEquals(true, $responseDataAddToCart['success']);
+        $this 
+            ->withHeader('Authorization', 'Bearer ' . $responseData['token'])
+            ->get('api/cart');
+        $responseDataCart = $response->json(); 
+        $this->assertEquals(1, $responseDataCart[0]['count']);
+        $this->assertEquals($newBook->id, $responseDataCart[0]['book_id']);
     }
 }
